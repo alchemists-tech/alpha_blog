ce9e302eca21bf3700c1c481e264c44a9fc0017c
spiknumb <ubuntu@ip-172-31-25-6.us-east-2.compute.internal>
2021-03-31 19:37:57 +0000

    added b crypt gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_141450) do
+ActiveRecord::Schema.define(version: 2021_03_31_192159) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_03_31_141450) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
